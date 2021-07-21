39969634dd4aa63ff6fc6d1d789c55ebc59e47ab
Priyesh <raj.priyesh448@gmail.com>
2021-07-21 18:15:14 +0530

    Removing Reference

---
+++ VatsaCart/VatsaCart/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VatsaCart.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Items> Items { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
